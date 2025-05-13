bbc7a37c362f6f44ee47bbb775ab06d799f17e1d
kprokup <kprokup2@illinois.edu>
2025-05-13 13:27:21 -0500

    Adding TOV and tidal love number functionality in IMRPhenomD_NRT_EOS
    Pushing current progress on adding QLIMR functionality to IMRPhenomD_NRT_EOS waveform files. This version compiles but integration of code with GWAT has not been fully tested and may cause issues if attempting to use the IMRPhenomD_NRT_EOS waveform. Currently there is no way to interface with the QLIMR functionality within the waveform template, I am working on adding this.

--- include/gwat/IMRPhenomD_NRT_EOS.h
+++ include/gwat/IMRPhenomD_NRT_EOS.h
@@ -2,6 +2,21 @@
 #define IMRPHENOMD_NRT_EOS
 #include "IMRPhenomD_NRT.h"
 #include "util.h"
+#include <gsl/gsl_integration.h>
+#include <gsl/gsl_spline.h>
+#include <iomanip>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <gsl/gsl_errno.h>
+#include <gsl/gsl_matrix.h>
+#include <gsl/gsl_odeiv2.h>
+
+using std::string;
+using std::vector;
 
 /*! \file
  */
@@ -30,5 +45,147 @@ public:
   */
 
 };
+
+// ****************************************************************************
+class Interpolation {
+  public:
+    // GSL-type variables for interpolation
+    gsl_interp_accel *acc;
+    gsl_spline       *spline;
+    gsl_interp_type  *type;
+  
+    // Vectors
+    vector<double> epsilon_col1;
+    vector<double> pressure_col2;
+  
+    // Size of data to be interpolated
+    size_t size;
+  
+    // Initializing GSL spline for interpolation
+    void initialize(gsl_interp_type *type, vector<double> x, vector<double> y);
+  
+    // Evaluate interpolated function at point x
+    double yofx(double x);
+  
+    // Evaluate derivative of interpolated function at point x
+    double dyofx(double x);
+  
+    // Free GSL spline and accelerator memory
+    void free();
+  };
+  // ****************************************************************************
+  
+  struct EOSinterpolation {
+  
+    // Interpolation type objects:
+  
+    Interpolation p_of_e; // Pressure as a function of energy density
+    Interpolation h_of_e; // Pseudo-enthalpy as a function of energy density
+    Interpolation h_of_p; // Enthalpy as a function of pressure
+    Interpolation e_of_h; // Energy density as a function of pseudo-enthalpy
+    Interpolation p_of_h; // Pressure as a function of pseudo-enthalpy
+  
+    // Vectors to store EoS data columns
+  
+    vector<double> e_vec;  // Energy density vector
+    vector<double> p_vec;  // Pressure vector
+    vector<double> h_vec;  // Enthalpy vector
+  };
+  
+  // ****************************************************************************
+  class EOS : public Interpolation {
+  public:
+    // Static structure for EoS to be used along the code 
+    static EOSinterpolation EoS;
+  
+    // Default constructor
+    EOS();
+  
+    // Parametric constructor: 
+    EOS(gsl_interp_type *type);
+  
+    // Method function to compute EoS in terms of pseudo-enthalpy (h)
+    void calculate_eos_of_h(vector<double> *epsilon, gsl_interp_type *type);
+  };
+  // ****************************************************************************
+  
+  struct Local_functions {
+      // Zeroth order local functions
+      Interpolation M_of_R;
+      Interpolation p_of_R;
+      Interpolation e_of_R;
+      Interpolation nu_of_R;
+    
+      // Second order local functions
+      Interpolation Y_of_R;
+    
+      // ######### Integration constants ########
+      // First order
+      double NS_Omega;
+    
+    };
+    
+    //--------------------------------- CLASS TOV ---------------------------------
+    class TOV : public EOS {
+    public:
+    
+      // Variables
+      double R_start;
+    
+      // Declaring structure variable fun of type Local_functions
+      Local_functions fun;
+    
+      // Declaring neutron star mass and radius!
+      double NS_R;
+      double NS_M;
+    
+      // Structure to hold initial conditions for TOV integrator
+      struct Initial_conditions_TOV {
+        double R_start;
+        double M_start; 
+        double h_start;
+        double h_istep;
+      };
+    
+      // Instance of Initial_conditions_TOV to store initial conditions
+      Initial_conditions_TOV IC_tov;
+    
+      // Method to calculate initial conditions based on central energy density
+      Initial_conditions_TOV IC_TOV(double epsilon_c);
+    
+      // TOV integrator method using pseudo-enthalpy (h) formulation
+      void TOV_Integrator(double epsilon_c, EOSinterpolation *eos);
+  
+      // Unit conversion
+      double adimensionalize(double value, string unit);
+    
+      // Method to calculate chemical potential as a function of radius µ(R)
+      double mu_of_R(double r);
+    
+      // Method to calculate baryon number density as a function of radius n(R)
+      double n_of_R(double r);
+    
+    };
+    //-----------------------------------------------------------------------------
+  
+    class Second_Order : public TOV {
+      public:
+        double NS_k2;  
+        double NS_Lbar;
+        
+        // ########################### Tidal Love number λ̄ #########################
+      
+        struct Initial_conditions_Y {
+          double Y_start;
+          double R_istep;
+        };
+      
+        Initial_conditions_Y IC_y;
+        Initial_conditions_Y IC_Y();
+      
+        // Integrator to obtain tidal love number: λ̄
+        void TidalLove_Integrator(Local_functions *fun);
+      
+      };
 
 #endif
--- src/IMRPhenomD_NRT_EOS.cpp
+++ src/IMRPhenomD_NRT_EOS.cpp
@@ -52,4 +52,465 @@ void IMRPhenomD_NRT_EOS<T>::get_m_love()
 
 
 template class IMRPhenomD_NRT_EOS<double>;
-template class IMRPhenomD_NRT_EOS<adouble>; 
+template class IMRPhenomD_NRT_EOS<adouble>;
+
+// *********************** QLIMR TOV and λ̄ FUNCTIONALITY ***********************
+
+EOSinterpolation EOS::EoS;
+EOS::EOS(){}; // Default EOS constructor
+
+// ----------------------- Parametric EOS constructor -------------------------
+EOS::EOS(gsl_interp_type *type) {
+
+  // Interpolate p(ε): initialize GSL interpolation spline
+  EoS.p_of_e.initialize(type, epsilon_col1, pressure_col2);
+
+  // Compute interpolated ε(h) and p(h) 
+  calculate_eos_of_h(&epsilon_col1, type);
+
+}
+// ----------------------------------------------------------------------------
+
+// ---------------------- Interpolation class methods -------------------------
+void Interpolation::initialize(gsl_interp_type *interp_type, vector<double> x,
+                               vector<double> y) {
+  type = interp_type;
+  // Size of the independent variable vector                            
+  size = x.size();    
+
+  // Allocating GSL interpolation accelerator
+  acc = gsl_interp_accel_alloc();
+
+  // Allocating GSL spline of specified type and size
+  spline = gsl_spline_alloc(type, size);
+
+  // Initializing GSL spline with given data points (x, y) and size
+  gsl_spline_init(spline, x.data(), y.data(), size);
+}
+
+// Function to calculate interpolated y value for a given x using GSL spline
+double Interpolation::yofx(double x) {
+  double result;
+  if (x >= 0.0) {
+    result = gsl_spline_eval(spline, x, acc);
+  } else {
+    result = 0.0;
+  }
+  return result;
+}
+
+// Function to calculate dy/dx of interpolated data using GSL spline
+double Interpolation::dyofx(double x) {
+  double result;
+  if (x > 0.0) {
+    result = gsl_spline_eval_deriv(spline, x, acc);
+  } else {
+    result = 0.0;
+  }
+  return result;
+}
+
+// Method to release memory of spline and accelerator
+void Interpolation::free() {
+  gsl_spline_free(spline);
+  gsl_interp_accel_free(acc);
+}
+//-----------------------------------------------------------------------------
+
+// ---------------------------- Defining dh/dε --------------------------------
+double enthalpy_integrand(double epsilon, void *params) {
+
+  double p = ((Interpolation *)params)->yofx(epsilon);
+  double cs2 = ((Interpolation *)params)->dyofx(epsilon);
+
+  return cs2 / (p + epsilon);
+}
+// ----------------------------------------------------------------------------
+
+//------------------- Integration: Finding ε(h) and p(h) ----------------------
+void EOS::calculate_eos_of_h(vector<double> *epsilon,
+                                 gsl_interp_type *type) {
+  double delta_h;
+  double error;
+  double h = 0.0;
+
+  // Integration workspace size
+  size_t integration_workspace_size = 100 * (epsilon->size());
+
+  // GSL variable type for integrating function
+  gsl_function F;
+
+  // Function to be integrated
+  F.function = &enthalpy_integrand;
+
+  // Passing object parameter which contains p(ε)
+  F.params = &EoS.p_of_e;
+
+  // Add h=0 and ε=0 according to definition of h
+  EoS.h_vec.push_back(0.0);
+  EoS.e_vec.push_back(0.0);
+  EoS.p_vec.push_back(0.0);
+
+  for (size_t i = 1; i < epsilon->size() - 1; i++) {
+
+    // Allocate memory for GSL integration workspace
+    gsl_integration_workspace *w =
+    gsl_integration_workspace_alloc(integration_workspace_size);
+
+    // Perform adaptive quadrature integration using GSL library
+    gsl_integration_qag(&F,
+                       (*epsilon)[i],     // Lower integration limit
+                       (*epsilon)[i + 1], // Upper integration limit
+                       1e-9,              // Absolute tolerance
+                       1e-9,              // Relative tolerance
+                       1000,              // Maximal number of subintervals
+                       6,                 // Integration method (Gauss-Kronrod)
+                       w,                 // Work space
+                       &delta_h,          // Estimated step size
+                       &error             // Estimated integration error
+    );
+
+    h = h + delta_h;
+    EoS.h_vec.push_back(h);
+    EoS.e_vec.push_back((*epsilon)[i + 1]);
+    EoS.p_vec.push_back(EoS.p_of_e.yofx((*epsilon)[i + 1]));
+    gsl_integration_workspace_free(w);
+  }
+  
+  EoS.h_of_e.initialize(type, EoS.e_vec, EoS.h_vec); // Interpolate h(ε)
+  EoS.h_of_p.initialize(type, EoS.p_vec, EoS.h_vec); // Interpolate h(p)
+  EoS.e_of_h.initialize(type, EoS.h_vec, EoS.e_vec); // Interpolate ε(h)
+  EoS.p_of_h.initialize(type, EoS.h_vec, EoS.p_vec); // Interpolate p(h)
+}
+//-----------------------------------------------------------------------------
+
+// TOV equations (h-formulation)
+int TOV_equations(double h, const double y[], double f[], void *paraM_sol)
+{
+
+  // Recast input parameter pointer of the ODE system as an input object
+  EOSinterpolation *eos = (EOSinterpolation *)paraM_sol;
+
+  // EoS variables ε(h) and p(h) to be inserted into the ODE system
+  double e = eos->e_of_h.yofx(h);
+  double p = eos->p_of_h.yofx(h);
+
+  // Functions R(h) and M(h) to be solved
+  double R = y[0];
+  double M = y[1];
+
+  double x0 = 4 * M_PI * std::pow(R, 3);
+
+  // TOV equations in h-formulation form. Here, f[0]= dR/dh and f[1]= dM/dh.
+  f[0] = -R * (-2 * M + R) / (M + x0 * p);
+  f[1] = -e * x0 * (-2 * M + R) / (M + p * x0);
+
+  return GSL_SUCCESS;
+}
+
+// Initial conditions for TOVh system 
+TOV::Initial_conditions_TOV TOV::IC_TOV(double epsilon_c)
+{
+
+  // Values at exactly the center
+  double pc = EoS.p_of_e.yofx(epsilon_c);
+  double hc = EoS.h_of_e.yofx(epsilon_c);
+  double Cs2 = EoS.p_of_e.dyofx(epsilon_c);
+
+  // ################## Asymptotic solutions ###################
+
+  double R = R_start;
+
+  double M = (4.0 / 3.0) * M_PI * epsilon_c * pow(R, 3);
+
+  // -----------------------------------------------------------
+
+  double p = pc - (2.0 / 3.0) * M_PI * pow(R, 2) * (epsilon_c + 3.0 * pc) *
+                      (epsilon_c + pc);
+
+  double e = epsilon_c - (2.0 / 3.0) * M_PI * pow(R, 2) *
+                             (epsilon_c + 3.0 * pc) * (epsilon_c + pc) / Cs2;
+
+  // ####################### dM/dh, dR/dh at R=Rε ########################
+
+  double dRdh = -(R * (R - 2.0 * M)) / (M + 4.0 * M_PI * pow(R, 3) * p);
+
+  double dMdh = 4.0 * M_PI * e * pow(R, 2) * dRdh;
+
+  // Filling initial conditions and initial step size into structure
+  IC_tov.R_start = R_start;
+  IC_tov.M_start = M;
+  IC_tov.h_start = hc - (2.0 / 3.0) * M_PI * pow(R, 2) * (epsilon_c + 3.0 * p);
+  IC_tov.h_istep = 0.1 * std::min(abs(R / dRdh), std::abs(M / dMdh));
+
+  // cout << min(abs(R / dRdh), abs(M / dMdh)) << endl;
+  // cout << hc << endl;
+  // //exit(1);
+
+  return IC_tov;
+}
+
+//  TOV Integrator using pseudo-enthalpy (h) formulation
+void TOV::TOV_Integrator(double epsilon_c, EOSinterpolation *eos) {
+
+  // Number of dependent variables of the ODE system to be solved
+  const int dim = 2;
+
+  // Defining GSL variables: system, step, control and evolve 
+  gsl_odeiv2_system sys = {TOV_equations, NULL, dim, eos};
+  gsl_odeiv2_step *s = gsl_odeiv2_step_alloc(gsl_odeiv2_step_rkf45, dim);
+  gsl_odeiv2_control *c = gsl_odeiv2_control_y_new(1e-10, 1e-10);
+  gsl_odeiv2_evolve *e = gsl_odeiv2_evolve_alloc(dim);
+  
+  // Obtain initial conditions
+  IC_TOV(epsilon_c);
+
+  // Initial conditions array for r and m at R_start
+  double y[2] = {IC_tov.R_start, IC_tov.M_start};
+
+  // Initial value of h to begin the integration
+  double h = IC_tov.h_start;
+
+  // Initial step size for the GSL adaptative step size algorithm
+  double h_istep = -1.0 * IC_tov.h_istep;
+
+  // Final value of h to stop integration
+  double h_stop = 0.0;
+
+  // Solution vectors to store h, R, M, p, e and ν at each step in h
+  vector<double> h_sol, R_sol, M_sol, p_sol, e_sol, nu_sol;
+
+  // Add initial conditions at h_start
+  h_sol.push_back(h);
+  R_sol.push_back(y[0]);
+  M_sol.push_back(y[1]);
+  p_sol.push_back(eos->p_of_e.yofx((eos->e_of_h.yofx(h))));
+  e_sol.push_back(eos->e_of_h.yofx(h));
+
+  // Integration loop from h = h_start up to the surface when h = 0.0
+  while (h > h_stop) {
+
+    // Solve the ODE system using GSL with rkf45 method at each step
+    int status = gsl_odeiv2_evolve_apply(e, c, s,
+                 &sys, &h, h_stop, &h_istep, y);
+
+    // Stop solving if something's wrong with the numerical integration
+    if (status != GSL_SUCCESS) break;
+      
+    // Store h values to be used for finding ν(h) */
+    h_sol.push_back(h);
+
+    // Store radial distance (R) after each step
+    R_sol.push_back(y[0]);
+
+    // Store enclosed mass (M) after each step
+    M_sol.push_back(y[1]);
+
+    // Store pressure (p) after each step
+    p_sol.push_back(eos->p_of_e.yofx((eos->e_of_h.yofx(h))));
+
+    // Store energy density (ε) after each step
+    e_sol.push_back(eos->e_of_h.yofx(h));
+  }
+
+  // // Check that integration indeed goes up to h = 0 where p = 0. 
+  // cout << "h = " << h  << " " << "p = " << ps.back() << " " << "e = " <<
+  // es.back()  << endl; exit(1);
+
+  // Free GSL variables of the integrator
+  gsl_odeiv2_evolve_free(e);
+  gsl_odeiv2_control_free(c);
+  gsl_odeiv2_step_free(s);
+
+  // Storing total mass M = m(h=0) and radius R = r(h=0) 
+  NS_R = y[0];
+  NS_M = y[1];
+
+  // Obtaining analytical solution for ν(h)
+  for (size_t i = 0; i < h_sol.size(); i++) {
+    nu_sol.push_back(log(1 - 2.0 * (NS_M / NS_R)) - 2 * h_sol[i]);
+  }
+
+  // Initialize GSL interpolation spline for m(r) solution
+  fun.M_of_R.initialize((gsl_interp_type *)gsl_interp_steffen, R_sol, M_sol);
+
+  // Initialize GSL interpolation spline for p(r) solution
+  fun.p_of_R.initialize((gsl_interp_type *)gsl_interp_steffen, R_sol, p_sol);
+
+  // Initialize GSL interpolation spline for ε(r) solution
+  fun.e_of_R.initialize((gsl_interp_type *)gsl_interp_steffen, R_sol, e_sol);
+
+  // Initialize GSL interpolation spline for nu(r) solution
+  fun.nu_of_R.initialize((gsl_interp_type *)gsl_interp_steffen, R_sol, nu_sol);
+};
+
+double TOV::adimensionalize(double value, string unit) {
+    double factor;
+  
+    if (unit == "MeV/fm^3") {
+      factor = 1.0 / 346933.783551;
+    } else if (unit == "km") {
+      factor = 1.0 / 1.47663;
+    } else if (unit == "g/cm^3") {
+      factor = 1.0 / 6.17625e+17;
+    } else if (unit == "MeV"){
+      factor = 8.96162e-61;   
+    } else if (unit == "1/fm^3"){
+      factor = 3.216297e54;  
+    } else if (unit == "-") {
+      factor = 1.0;
+    } else {
+      std::cout << "no unit match to adimensionalize" << std::endl;
+      exit(0);
+    }
+    
+    return factor * value;
+  }
+
+// Chemical potential as a function of radius µ(R)
+double TOV::mu_of_R(double R){
+
+  double mu_Fe;
+  double nu = fun.nu_of_R.yofx(R);
+  double mu_of_R;
+
+  // See Input_QLIMR::adimensionalize in A_Input.cpp
+  mu_Fe = adimensionalize(930.54, "MeV" );
+  mu_of_R = mu_Fe * sqrt((1.0 - 2.0 * (NS_M/NS_R))) * exp(-nu);
+
+  return mu_of_R;
+}
+
+// Baryon number density as a function of radius n(R)
+double TOV::n_of_R(double R){
+
+  double mu_Fe;
+  double p = fun.p_of_R.yofx(R);
+  double e = fun.e_of_R.yofx(R);
+  double nu = fun.nu_of_R.yofx(R);
+  double mu_of_R;
+  double n_of_R;
+
+  mu_Fe = adimensionalize(930.54, "MeV" );
+  mu_of_R = mu_Fe * sqrt((1.0 - 2.0 * (NS_M/NS_R))) * exp(-nu);
+
+  n_of_R = (p + e) / mu_of_R;
+
+  return n_of_R;
+}
+
+// ############################################################################
+// ################################ λ̄ (l=2) ###################################
+// ############################################################################
+
+// ODE for the tidal love function Y
+int TidalLove_equation(double R, const double y[], double f[], void *params_ptr)
+{
+
+  // Function parameters of the ODE:
+  Local_functions *fun = (Local_functions *)params_ptr;
+
+  double e = fun->e_of_R.yofx(R);
+  double p = fun->p_of_R.yofx(R);
+  double M = fun->M_of_R.yofx(R);
+  double de = fun->e_of_R.dyofx(R);
+
+  double x0 = 1.0/R;
+  double x1 = 4*M_PI;
+  double x2 = std::pow(R, 3)*x1;
+  double x3 = 2*M - R;
+  double x4 = std::pow(R, 2);
+  double x5 = M_PI*x4;
+  double x6 = 5*e;
+
+  double Y = y[0];
+
+  f[0] =  -std::pow(Y, 2)*x0 + Y*(4*x5*(-e + p) + 1)/x3 + de*x2/(M + p*x2) 
+          + x0*(4*std::pow(M, 2) + 4*M*R*(2*x5*(13*p + x6) - 3) 
+          + std::pow(R, 4)*x1*(p*(16*p*x5 - 9) - x6) + 6*x4)/std::pow(x3, 2);
+
+  return GSL_SUCCESS;
+}
+
+// Initial Conditions for Y
+Second_Order::Initial_conditions_Y Second_Order::IC_Y()
+{
+
+  double R = R_start;
+
+  // Defining initial step size for adaptative method
+  IC_y.R_istep = R/100;
+
+  return IC_y;
+}
+
+// Integrator for Y
+void Second_Order::TidalLove_Integrator(Local_functions *fun)
+{
+
+  const int dim = 1;
+
+  gsl_odeiv2_system sys = {TidalLove_equation, NULL, dim, fun};
+  gsl_odeiv2_step *s = gsl_odeiv2_step_alloc(gsl_odeiv2_step_rkf45, dim);
+  gsl_odeiv2_control *c = gsl_odeiv2_control_y_new(1e-12, 1e-12);
+  gsl_odeiv2_evolve *e = gsl_odeiv2_evolve_alloc(dim);
+
+  // Computing initial conditions of the ODE
+  IC_Y();
+
+  double y[1] = {IC_y.Y_start};
+  double R = R_start;
+  double R_istep = IC_y.R_istep;
+
+  // Defining step and solution vectors
+  vector<double> R_sol, Y_sol;
+
+  // Adding initial condition to the solution vector
+  R_sol.push_back(R);
+  Y_sol.push_back(IC_y.Y_start);
+
+  // Solving the ODE over the specified range
+  while (R < NS_R)
+  {
+    int status = gsl_odeiv2_evolve_apply(e, c, s, &sys, &R, NS_R, &R_istep, y);
+
+    if (status != GSL_SUCCESS)
+      break;
+
+    R_sol.push_back(R);
+    Y_sol.push_back(y[0]);
+    // cout << R << " " << y[0] << endl;
+   
+  }
+
+  gsl_odeiv2_evolve_free(e);
+  gsl_odeiv2_control_free(c);
+  gsl_odeiv2_step_free(s);
+
+  // Evaluating solution Y(R) at the NS surface: NS_Y
+  double NS_Y = y[0];
+
+  // Useful definitions for faster calculations
+  double C = NS_M / NS_R;
+  double C2 = C * C;
+  double C3 = C * C2;
+  double C4 = C2 * C2;
+  double C5 = C * C4;
+  double k = (1.0 - 2.0 * C) * (1.0 - 2.0 * C);
+
+  // Dimensionless tidal apsidal constant NS_k2: k2 [-]
+  NS_k2 = (8.0 / 5.0) * k * C5 * (2.0 * C * (NS_Y - 1.0) - NS_Y + 2.0) *
+          (1.0 / (2.0 * C *
+                      (4 * (NS_Y + 1.0) * C4 + (6.0 * NS_Y - 4.0) * C3 +
+                       (26.0 - 22.0 * NS_Y) * C2 +
+                       3.0 * (5.0 * NS_Y - 8.0) * C - 3.0 * NS_Y + 6.0) -
+                  3.0 * k * (2 * C * (NS_Y - 1.0) - NS_Y + 2.0) *
+                      log(1.0 / (1.0 - 2.0 * C))));
+
+  // Dimensionless tidal love number NS_Lbar: λ̄ [-]
+  NS_Lbar = (2.0 / 3.0) * NS_k2 * (1 / C5);
+
+  // Initialize GSL interpolation spline for Y(r) solution
+  fun->Y_of_R.initialize((gsl_interp_type *)gsl_interp_steffen, R_sol, Y_sol);
+  
+}

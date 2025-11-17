c9abb8514ef148bb0df776174e2574c154756f90
Peter <peter@paikman.com>
2025-11-17 06:35:11 +0000

    removed directory "testing"

--- testing/thread_pooling/temp/include/test3.tpp
+++
@@ -1,5 +0,0 @@
-template <typename data_type>
-data_type test3<data_type>::function(data_type a)
-{
-	return a*a;
-}
--- testing/thread_pooling/temp/src/test2.cpp
+++
@@ -1,5 +0,0 @@
-#include "test.h"
-#include <adolc/adouble.h>
-#undef data_type
-#define data_type adouble
-#include "test.cpp"
